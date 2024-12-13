81d31ad39537fde10345784445dee42194aed58d
AminSBU <omid.elc88@gmail.com>
2024-12-13 11:44:53 +0330

    modification of inputs

--- 03_JS_Calculator/my-app-calculator/src/App.js
+++ 03_JS_Calculator/my-app-calculator/src/App.js
@@ -5,44 +5,44 @@ function App() {
     <div className="container">
       <div className="calculator">
         <form action="">
-          <div>
+          <div className="CalculatorText">
             <input type="text">
             
             </input>
           </div>
 
           <div>
-            <input type="button" value="C"></input>
-            <input type="button" value="+-"></input>
-            <input type="button" value="%"></input>
-            <input type="button" value="/"></input>
+            <input type="button" value="C"/>
+            <input type="button" value="+-"/>
+            <input type="button" value="%"/>
+            <input type="button" value="/"/>
           </div>
 
           <div>
-            <input type="button" value="7"></input>
-            <input type="button" value="8"></input>
-            <input type="button" value="9"></input>
-            <input type="button" value="X"></input>
+            <input type="button" value="7"/>
+            <input type="button" value="8"/>
+            <input type="button" value="9"/>
+            <input type="button" value="X"/>
           </div>
 
           <div>
-            <input type="button" value="4"></input>
-            <input type="button" value="5"></input>
-            <input type="button" value="6"></input>
-            <input type="button" value="-"></input>
+            <input type="button" value="4"/>
+            <input type="button" value="5"/>
+            <input type="button" value="6"/>
+            <input type="button" value="-"/>
           </div>
 
           <div>
-            <input type="button" value="1"></input>
-            <input type="button" value="2"></input>
-            <input type="button" value="3"></input>
-            <input type="button" value="+"></input>
+            <input type="button" value="1"/>
+            <input type="button" value="2"/>
+            <input type="button" value="3"/>
+            <input type="button" value="+"/>
           </div>
 
           <div>
-            <input type="button" value="0"></input>
-            <input type="button" value="."></input>
-            <input type="button" value="="></input>
+            <input type="button" value="0"/>
+            <input type="button" value="."/>
+            <input type="button" value="=" className='equal' />
           </div>
         </form>
       </div>
