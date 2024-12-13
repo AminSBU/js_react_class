13b8e4f322d0df668344096b30d126059aaf3557
AminSBU <omid.elc88@gmail.com>
2024-12-13 11:59:00 +0330

    set limit for number of digits in display

--- 03_JS_Calculator/my-app-calculator/src/App.js
+++ 03_JS_Calculator/my-app-calculator/src/App.js
@@ -4,50 +4,54 @@ import './App.css';
 function App() {
   const [value, setValue] = useState('');
 
+  const handleButtonClick = (digit) => {  
+    if (value.length < 9) {  
+      setValue(value + digit);  
+    }  
+  };  
+
   return (
     <div className="container">
       <div className="calculator">
-        <form action="">
-          <div className="CalculatorText">
-            <input type="text" value={value}>
-            
-            </input>
-          </div>
+      <form action="">  
+          <div className="CalculatorText">  
+            <input type="text" value={value} readOnly />  
+          </div>  
 
-          <div>
-            <input type="button" value="C"/>
-            <input type="button" value="+-"/>
-            <input type="button" value="%"/>
-            <input type="button" value="/"/>
-          </div>
+          <div>  
+            <input type="button" value="C" />  
+            <input type="button" value="+-" />  
+            <input type="button" value="%" />  
+            <input type="button" value="/" />  
+          </div>  
 
-          <div>
-            <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="X" onClick={e => setValue(value + e.target.value)}/>
-          </div>
+          <div>  
+            <input type="button" value="7" onClick={() => handleButtonClick('7')} />  
+            <input type="button" value="8" onClick={() => handleButtonClick('8')} />  
+            <input type="button" value="9" onClick={() => handleButtonClick('9')} />  
+            <input type="button" value="X" onClick={() => handleButtonClick('X')} />  
+          </div>  
 
-          <div>
-            <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="5" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="6" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
-          </div>
+          <div>  
+            <input type="button" value="4" onClick={() => handleButtonClick('4')} />  
+            <input type="button" value="5" onClick={() => handleButtonClick('5')} />  
+            <input type="button" value="6" onClick={() => handleButtonClick('6')} />  
+            <input type="button" value="-" onClick={() => handleButtonClick('-')} />  
+          </div>  
 
-          <div>
-            <input type="button" value="1" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="2" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="3" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="+"/>
-          </div>
+          <div>  
+            <input type="button" value="1" onClick={() => handleButtonClick('1')} />  
+            <input type="button" value="2" onClick={() => handleButtonClick('2')} />  
+            <input type="button" value="3" onClick={() => handleButtonClick('3')} />  
+            <input type="button" value="+" onClick={() => handleButtonClick('+')} />  
+          </div>  
 
-          <div>
-            <input type="button" value="0" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="."/>
-            <input type="button" value="=" className='equal' />
-          </div>
-        </form>
+          <div>  
+            <input type="button" value="0" onClick={() => handleButtonClick('0')} />  
+            <input type="button" value="." />  
+            <input type="button" value="=" className='equal' />  
+          </div>  
+        </form>  
       </div>
     </div>
   );
