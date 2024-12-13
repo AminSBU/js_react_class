6f4090db03867930d4d54548928a98c66f9723b9
AminSBU <omid.elc88@gmail.com>
2024-12-13 11:56:45 +0330

    display numbers in CalculatorText

--- 03_JS_Calculator/my-app-calculator/src/App.js
+++ 03_JS_Calculator/my-app-calculator/src/App.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+
   return (
     <div className="container">
       <div className="calculator">
         <form action="">
           <div className="CalculatorText">
-            <input type="text">
+            <input type="text" value={value}>
             
             </input>
           </div>
@@ -19,28 +22,28 @@ function App() {
           </div>
 
           <div>
-            <input type="button" value="7"/>
-            <input type="button" value="8"/>
-            <input type="button" value="9"/>
-            <input type="button" value="X"/>
+            <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="X" onClick={e => setValue(value + e.target.value)}/>
           </div>
 
           <div>
-            <input type="button" value="4"/>
-            <input type="button" value="5"/>
-            <input type="button" value="6"/>
-            <input type="button" value="-"/>
+            <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="5" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="6" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
           </div>
 
           <div>
-            <input type="button" value="1"/>
-            <input type="button" value="2"/>
-            <input type="button" value="3"/>
+            <input type="button" value="1" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="2" onClick={e => setValue(value + e.target.value)}/>
+            <input type="button" value="3" onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value="+"/>
           </div>
 
           <div>
-            <input type="button" value="0"/>
+            <input type="button" value="0" onClick={e => setValue(value + e.target.value)}/>
             <input type="button" value="."/>
             <input type="button" value="=" className='equal' />
           </div>
