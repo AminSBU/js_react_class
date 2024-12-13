224123eb93403170a3badf39d4ac2dbd73a59f4c
AminSBU <omid.elc88@gmail.com>
2024-12-13 10:50:29 +0330

    add all input buutons

--- 03_JS_Calculator/my-app-calculator/src/App.js
+++ 03_JS_Calculator/my-app-calculator/src/App.js
@@ -10,6 +10,40 @@ function App() {
             
             </input>
           </div>
+
+          <div>
+            <input type="button" value="C"></input>
+            <input type="button" value="+-"></input>
+            <input type="button" value="%"></input>
+            <input type="button" value="/"></input>
+          </div>
+
+          <div>
+            <input type="button" value="7"></input>
+            <input type="button" value="8"></input>
+            <input type="button" value="9"></input>
+            <input type="button" value="X"></input>
+          </div>
+
+          <div>
+            <input type="button" value="4"></input>
+            <input type="button" value="5"></input>
+            <input type="button" value="6"></input>
+            <input type="button" value="-"></input>
+          </div>
+
+          <div>
+            <input type="button" value="1"></input>
+            <input type="button" value="2"></input>
+            <input type="button" value="3"></input>
+            <input type="button" value="+"></input>
+          </div>
+
+          <div>
+            <input type="button" value="0"></input>
+            <input type="button" value="."></input>
+            <input type="button" value="="></input>
+          </div>
         </form>
       </div>
     </div>
